e195ab010b3dded7e91be5e8955a32eddd5c1b8d
Will Charczuk <will@blendlabs.com>
2016-01-20 23:01:38 -0800

    jarvis getting better.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"regexp"
 	"sync"
 	"time"
 
@@ -10,6 +11,8 @@ import (
 )
 
 var _usersLookup map[string]slack.User
+var _channelsLookup map[string]slack.Channel
+var _botId string
 
 func TOKEN() string {
 	return os.Getenv("SLACK_API_TOKEN")
@@ -23,8 +26,10 @@ func main() {
 	})
 
 	client.Listen(slack.EVENT_MESSAGE, func(m *slack.Message, c *slack.Client) {
-		user := findUser(m.User)
-		logf("Slack Message: %s - %s", user.Name, m.Text)
+		resErr := doResponse(m, c)
+		if resErr != nil {
+			log(resErr)
+		}
 	})
 
 	session, err := client.Start()
@@ -32,11 +37,37 @@ func main() {
 		fmt.Printf("%v\n", err)
 	}
 
+	_botId = session.Self.Id
 	_usersLookup = createUsersLookup(session)
+	_channelsLookup = createChannelLookup(session)
 
 	wg := sync.WaitGroup{}
 	wg.Add(1)
 	wg.Wait()
+}
+
+func doResponse(m *slack.Message, c *slack.Client) error {
+	user := findUser(m.User)
+	channel := findChannel(m.Channel)
+	message := m.Text
+
+	logf("incomping message: #%s - %s - %s", channel.Name, user.Name, message)
+	if channel.Name == "bot-test" {
+		if like(message, fmt.Sprintf("@%s", _botId)) {
+			return sayf(c, channel.Id, "hello %s", user.Profile.FirstName)
+		}
+	}
+	return nil
+}
+
+func sayf(c *slack.Client, channel, format string, components ...interface{}) error {
+	logf("outgoing message : #%s - %s", channel, fmt.Sprintf(format, components...))
+	return c.Sayf(channel, format, components...)
+}
+
+func like(corpus, expr string) bool {
+	matched, _ := regexp.Match(expr, []byte(corpus))
+	return matched
 }
 
 func findUser(userId string) *slack.User {
@@ -46,11 +77,27 @@ func findUser(userId string) *slack.User {
 	return nil
 }
 
+func findChannel(channelId string) *slack.Channel {
+	if channel, hasChannel := _channelsLookup[channelId]; hasChannel {
+		return &channel
+	}
+	return nil
+}
+
 func createUsersLookup(session *slack.Session) map[string]slack.User {
 	lookup := map[string]slack.User{}
 	for x := 0; x < len(session.Users); x++ {
 		user := session.Users[x]
 		lookup[user.Id] = user
+	}
+	return lookup
+}
+
+func createChannelLookup(session *slack.Session) map[string]slack.Channel {
+	lookup := map[string]slack.Channel{}
+	for x := 0; x < len(session.Channels); x++ {
+		channel := session.Channels[x]
+		lookup[channel.Id] = channel
 	}
 	return lookup
 }
