b4a270fda92ac86ca819536cee340235c67f6eb4
Will Charczuk <will@blendlabs.com>
2016-01-21 00:45:22 -0800

    likes to nullref, sadness.

--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+	"net/url"
 	"os"
 	"regexp"
+	"strings"
 	"sync"
 	"time"
 
+	"github.com/PuerkitoBio/goquery"
+	"github.com/blendlabs/go-request"
 	"github.com/wcharczuk/go-slack"
 )
 
@@ -22,7 +26,7 @@ func main() {
 	client := slack.Connect(TOKEN())
 
 	client.Listen(slack.EVENT_HELLO, func(m *slack.Message, c *slack.Client) {
-		log("Slack: Connected")
+		log("connected")
 	})
 
 	client.Listen(slack.EVENT_MESSAGE, func(m *slack.Message, c *slack.Client) {
@@ -50,19 +54,59 @@ func doResponse(m *slack.Message, c *slack.Client) error {
 	user := findUser(m.User)
 	channel := findChannel(m.Channel)
 	message := m.Text
+	messageLessMentions := withoutMentions(message)
 
-	logf("incomping message: #%s - %s - %s", channel.Name, user.Name, message)
+	logf("incoming message: #%s - %s - %s", channel.Name, user.Name, message)
 	if channel.Name == "bot-test" {
-		if like(message, fmt.Sprintf("@%s", _botId)) {
-			return sayf(c, channel.Id, "hello %s", user.Profile.FirstName)
+		if isMention(message) {
+			if strings.HasPrefix(messageLessMentions, "food") {
+				queryPieces := strings.Split(messageLessMentions, " ")[1:]
+				results := amazonFoodSearch(strings.Join(queryPieces, " "))
+				if len(results) != 0 {
+					return sayf(c, channel.Id, results[0])
+				} else {
+					return sayf(c, channel.Id, "No Results for Food Query\n>%s", messageLessMentions)
+				}
+
+			} else {
+				return sayf(c, channel.Id, "I don't know how to respond to this\n>%s", messageLessMentions)
+			}
 		}
 	}
 	return nil
 }
 
-func sayf(c *slack.Client, channel, format string, components ...interface{}) error {
-	logf("outgoing message : #%s - %s", channel, fmt.Sprintf(format, components...))
-	return c.Sayf(channel, format, components...)
+func isMention(message string) bool {
+	return like(message, fmt.Sprintf("<@%s>", _botId))
+}
+
+func withoutMentions(message string) string {
+	output := ""
+	state := 0
+	for _, c := range message {
+		switch state {
+		case 0:
+			if c == rune("<"[0]) {
+				state = 1
+			} else {
+				output = output + string(c)
+			}
+		case 1:
+			if c == rune(">"[0]) {
+				state = 2
+			}
+		case 2:
+			if c == rune(":"[0]) {
+				state = 2
+			} else if c == rune(" "[0]) {
+				state = 0
+			} else {
+				state = 0
+				output = output + string(c)
+			}
+		}
+	}
+	return output
 }
 
 func like(corpus, expr string) bool {
@@ -102,6 +146,12 @@ func createChannelLookup(session *slack.Session) map[string]slack.Channel {
 	return lookup
 }
 
+func sayf(c *slack.Client, channelId, format string, components ...interface{}) error {
+	channel := findChannel(channelId)
+	logf("outgoing message: #%s - %s", channel.Name, fmt.Sprintf(format, components...))
+	return c.Sayf(channelId, format, components...)
+}
+
 func log(components ...interface{}) {
 	message := fmt.Sprint(components...)
 	fmt.Printf("%s - %s\n", time.Now().UTC().Format(time.RFC3339), message)
@@ -110,4 +160,31 @@ func log(components ...interface{}) {
 func logf(format string, components ...interface{}) {
 	message := fmt.Sprintf(format, components...)
 	fmt.Printf("%s - %s\n", time.Now().UTC().Format(time.RFC3339), message)
+}
+
+func amazonFoodSearch(query string) []string {
+	queryEscaped := url.QueryEscape(query)
+	queryFormat := "http://www.amazon.com/s/ref=nb_sb_noss_1?url=search-alias%3Dgrocery&field-keywords=%s"
+	fullQuery := fmt.Sprintf(queryFormat, queryEscaped)
+
+	results, fetchErr := request.NewRequest().AsGet().WithHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.82 Safari/537.36").WithUrl(fullQuery).FetchString()
+	if fetchErr != nil {
+		log(fetchErr)
+		return []string{}
+	}
+
+	doc, docErr := goquery.NewDocumentFromReader(strings.NewReader(results))
+
+	if docErr != nil {
+		log(docErr)
+		return []string{}
+	}
+
+	products := []string{}
+	doc.Find(".a-link-normal").Each(func(i int, s *goquery.Selection) {
+		href, _ := s.Attr("href")
+		products = append(products, href)
+	})
+
+	return products
 }
