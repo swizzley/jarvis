d97d464bbae811758002f29851d9d79dd6aaa053
Will Charczuk <will@clotheshor.se>
2016-01-21 15:05:15 -0800

    snapshot.

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ import (
 var _usersLookup map[string]slack.User
 var _channelsLookup map[string]slack.Channel
 var _botId string
+var _orders []order
 
 func TOKEN() string {
 	return os.Getenv("SLACK_API_TOKEN")
@@ -33,7 +34,6 @@ func main() {
 	})
 
 	client.Listen(slack.EVENT_MESSAGE, func(m *slack.Message, c *slack.Client) {
-		logf("message hook fired")
 		resErr := doResponse(m, c)
 		if resErr != nil {
 			log(resErr)
@@ -48,6 +48,7 @@ func main() {
 	_botId = session.Self.Id
 	_usersLookup = createUsersLookup(session)
 	_channelsLookup = createChannelLookup(session)
+	_orders = []order{}
 
 	wg := sync.WaitGroup{}
 	wg.Add(1)
@@ -64,24 +65,49 @@ func doResponse(m *slack.Message, c *slack.Client) error {
 		return nil
 	}
 
-	logf("incoming message: #%s - %s - %s", channel.Name, user.Name, message)
+	logf("=> #%s - %s: %s", channel.Name, user.Name, message)
 	if isMention(fullMessage) {
-		if strings.HasPrefix(message, "food") {
-			results := amazonFoodSearch(lessFirst(message))
-			if len(results) != 0 {
-				firstProduct := results[0]
-				return sayf(c, channel.Id, "I found the following product, it costs %s\n>%s", firstProduct.price, firstProduct.url)
-			} else {
-				return sayf(c, channel.Id, "No Results for Food Query\n>%s", message)
-			}
+		if like(message, "^search") {
+			return doSearch(c, channel, user, message)
+		} else if likeAny(message, []string{"^order ", "^add", "^include"}) {
+
+		} else if likeAny(message, []string{"^orders", "^list orders", "^show orders"}) {
+			return doListOrders(c, channel, user, message)
 		} else if isSalutation(message) {
-			salutation := []string{"Hey %s", "Hi %s", "Hello %s", "Ohayo Gozamaisu %s", "Salut %s", "Yo %s"}
-			return sayf(c, channel.Id, random(salutation), user.Profile.FirstName)
+			return doSalutation(c, channel, user, message)
 		} else {
-			return sayf(c, channel.Id, "I don't know how to respond to this\n>%s", message)
+			return doUnknown(c, channel, user, message)
 		}
 	}
 	return nil
+}
+
+func doSearch(c *slack.Client, channel *slack.Channel, user *slack.User, message string) error {
+	results := amazonFoodSearch(lessFirst(message))
+	if len(results) != 0 {
+		firstProduct := results[0]
+		return sayf(c, channel.Id, "I found the following product, it costs %s\n>%s", firstProduct.price, firstProduct.url)
+	} else {
+		return sayf(c, channel.Id, "No Results for Food Query\n>%s", message)
+	}
+}
+
+func doListOrders(c *slack.Client, channel *slack.Channel, user *slack.User, message string) error {
+	output := fmt.Sprintf("I have %d products listed to order:\n", len(_orders))
+	for _, product := range _orders {
+		user := findUser(product.ordered_by)
+		output = output + fmt.Sprintf("%s has asked that we order:\n>%s\n", user.Profile.FirstName, product.product.name)
+	}
+	return sayf(c, channel.Id, output)
+}
+
+func doSalutation(c *slack.Client, channel *slack.Channel, user *slack.User, message string) error {
+	salutation := []string{"Hey %s", "Hi %s", "Hello %s", "Ohayo Gozaimasu %s", "Salut %s", "Bonjour %s", "yo %s", "sup %s"}
+	return sayf(c, channel.Id, random(salutation), user.Profile.FirstName)
+}
+
+func doUnknown(c *slack.Client, channel *slack.Channel, user *slack.User, message string) error {
+	return sayf(c, channel.Id, "I don't know how to respond to this\n>%s", message)
 }
 
 func random(messages []string) string {
@@ -94,6 +120,14 @@ func isMention(message string) bool {
 
 func isSalutation(message string) bool {
 	return likeAny(message, []string{"^hello", "^hi", "^greetings", "^hey"})
+}
+
+func isAsking(message string) bool {
+	return likeAny(message, []string{"would it be possible", "can you", "would you", "is it possible"})
+}
+
+func isPolite(message string) bool {
+	return likeAny(message, []string{"please", "thanks", "([^.?!]*)\\?"})
 }
 
 func lessMentions(message string) string {
@@ -158,6 +192,10 @@ func findChannel(channelId string) *slack.Channel {
 	return nil
 }
 
+func addOrder(u *slack.User, product amazonProduct) {
+	_orders = append(_orders, order{timestamp: time.Now().UTC(), ordered_by: u.Id, product: product})
+}
+
 func createUsersLookup(session *slack.Session) map[string]slack.User {
 	lookup := map[string]slack.User{}
 	for x := 0; x < len(session.Users); x++ {
@@ -178,7 +216,7 @@ func createChannelLookup(session *slack.Session) map[string]slack.Channel {
 
 func sayf(c *slack.Client, channelId, format string, components ...interface{}) error {
 	channel := findChannel(channelId)
-	logf("outgoing message: #%s - %s", channel.Name, fmt.Sprintf(format, components...))
+	logf("<= #%s - jarvis: %s", channel.Name, fmt.Sprintf(format, components...))
 	return c.Sayf(channelId, format, components...)
 }
 
@@ -192,7 +230,14 @@ func logf(format string, components ...interface{}) {
 	fmt.Printf("%s - %s\n", time.Now().UTC().Format(time.RFC3339), message)
 }
 
+type order struct {
+	timestamp  time.Time
+	ordered_by string
+	product    amazonProduct
+}
+
 type amazonProduct struct {
+	id       string
 	name     string
 	price    string
 	url      string
@@ -202,7 +247,7 @@ type amazonProduct struct {
 func amazonFoodSearch(query string) []amazonProduct {
 	products := []amazonProduct{}
 	queryEscaped := url.QueryEscape(query)
-	queryFormat := "http://www.amazon.com/s/?url=search-alias%%3Dgrocery&field-keywords=%s"
+	queryFormat := "http://www.amazon.com/s/?field-keywords=%s"
 	fullQuery := fmt.Sprintf(queryFormat, queryEscaped)
 
 	results, fetchErr := request.NewRequest().AsGet().WithHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.82 Safari/537.36").WithUrl(fullQuery).FetchString()
@@ -224,12 +269,13 @@ func amazonFoodSearch(query string) []amazonProduct {
 
 	doc.Find("li.s-result-item").Each(func(i int, s *goquery.Selection) {
 		link := s.Find("a.s-access-detail-page").First()
-		href, _ := link.Attr("href")
-		price := s.Find("span.a-color-price")
-
+		price := s.Find("span.a-color-price").First()
 		prime := s.Find("i.a-icon-prime").First()
 
+		href, _ := link.Attr("href")
+
 		product := amazonProduct{}
+		product.id = util.UUID_v4().ToShortString()
 		product.url = href
 		product.name = link.Text()
 		product.price = price.Text()
