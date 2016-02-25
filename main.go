bac6f27964690db627eafc740f232cdec0d660c7
Will Charczuk <will@clotheshor.se>
2016-02-25 14:56:45 -0800

    blowing away vendored deps.

--- main.go
+++ main.go
@@ -7,8 +7,8 @@ import (
 	"strings"
 	"time"
 
-	"github.com/wcharczuk/jarvis-cli/Godeps/_workspace/src/github.com/blendlabs/go-util"
-	"github.com/wcharczuk/jarvis-cli/Godeps/_workspace/src/github.com/dlintw/goconf"
+	"github.com/blendlabs/go-util"
+	"github.com/dlintw/goconf"
 	"github.com/wcharczuk/jarvis-cli/jarvis"
 )
 
@@ -26,9 +26,8 @@ func port() string {
 	envPort := os.Getenv("PORT")
 	if len(envPort) == 0 {
 		return "8888"
-	} else {
-		return envPort
 	}
+	return envPort
 }
 
 func main() {
@@ -89,19 +88,19 @@ func startStatusServer(bots []*jarvis.JarvisBot) {
 	http.ListenAndServe(":"+port(), nil)
 }
 
-func injectBots(bots []*jarvis.JarvisBot, h botAwareHttpHandlerFunc) http.HandlerFunc {
+func injectBots(bots []*jarvis.JarvisBot, h botAwareHTTPHandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		h(bots, w, r)
 	}
 }
 
-type botAwareHttpHandlerFunc func(bots []*jarvis.JarvisBot, w http.ResponseWriter, r *http.Request)
+type botAwareHTTPHandlerFunc func(bots []*jarvis.JarvisBot, w http.ResponseWriter, r *http.Request)
 
 func statusHandler(bots []*jarvis.JarvisBot, w http.ResponseWriter, r *http.Request) {
 	for _, bot := range bots {
 		statusText := fmt.Sprintf("Jarvis is running and listening to the following channels (%s):\n", bot.OrganizationName)
-		for _, channelId := range bot.Client.ActiveChannels {
-			channel := bot.FindChannel(channelId)
+		for _, channelID := range bot.Client.ActiveChannels {
+			channel := bot.FindChannel(channelID)
 			statusText = statusText + fmt.Sprintf("> #%s (%s)\n", channel.Name, channel.Id)
 		}
 		statusText = statusText + "\n"
--- vendor/github.com/blendlabs/go-chronometer/job.go
+++
@@ -1,7 +0,0 @@
-package chronometer
-
-type Job interface {
-	Name() string
-	Schedule() Schedule
-	Execute(ct *CancellationToken) error
-}
